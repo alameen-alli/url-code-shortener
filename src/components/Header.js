ed0470cdc707c8b7ab992dd94d7282938004fa7d
alameen-alli <allialameen8739@gmail.com>
2023-08-27 01:53:12 +0100

    remove files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import logo from "../../assets/images/logo.svg"
+import logo from "../assets/images/logo.svg"
+import hamburger from "../assets/images/hamburger.svg"
 
 const Header = () => {
   return (
     <header>
         <nav>
             <ul>
-                <li><img src={logo}/></li>
-                <li></li>
+                <li><img src={logo} alt='logo'/></li>
+                <li><img src={hamburger} alt='bar-icon'/></li>
             </ul>
         </nav>
     </header>
