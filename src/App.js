5c75b709d6e2a05d624e5e43ea1f64de6be3aff8
alameen-alli <allialameen8739@gmail.com>
2023-08-30 10:03:12 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-import Header from './components/Header';
+import './global.css';
+import Main from './components/Main/Main'
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Main/>
     </div>
   );
 }
